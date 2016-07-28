091435da4c6339c70e5ffa401f8f568b24f05a02
Albert Nguyen <Albert.la.nguyen@gmail.com>
2016-07-27 22:19:24 -0700

    User Model up and running

--- db/migrate/20160728045005_create_users.rb
+++ db/migrate/20160728045005_create_users.rb
@@ -3,8 +3,10 @@ class CreateUsers < ActiveRecord::Migration
     create_table :users do |t|
       t.string :username, null: false
       t.string :password_digest, null: false
+      t.string :session_token, null: false
 
       t.timestamps null: false
     end
+    add_index :users, :username
   end
 end
