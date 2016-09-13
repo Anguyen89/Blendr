891158e8d83aa4e0dd1d53fdbfec53c8b9632d3d
Albert Nguyen <Albert.la.nguyen@gmail.com>
2016-09-13 14:24:48 -0700

    updated css for profile view

---
+++ frontend/components/posts/search.jsx
@@ -0,0 +1,23 @@
+var React = require('react');
+var ProfileStore = require('../../stores/profile_store');
+
+
+var SearchBar = React.createClass({
+
+  // getInitialState(){
+  //   return ({ users: []})
+  // },
+  //
+  // componentDidMount(){
+  //   this.ProfileStoreListener = ProfileStore.addListener(this._onChange);
+  //   ProfileActions.all();
+  // },
+  //
+  // _onChange(){
+  //   this.setState({ users: ProfileStore.all})
+  // }
+
+});
+
+
+module.exports = SearchBar;
